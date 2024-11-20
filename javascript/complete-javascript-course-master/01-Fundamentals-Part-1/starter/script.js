6cda4ee1390d3abee9487d06197ec5fcc8d51698
Jin9991 <jinksem@gmail.com>
2024-11-20 16:44:20 +0900

    update

---
+++ javascript/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,5 @@
+let js = 'amazing';
+if ( js === 'amazing') alert("nice to meet you");
+
+40 + 2 + 4 - 100 ;
+console.log(40 + 2 + 4 - 100);
--- javascript/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ javascript/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,2 +1,56 @@
-'use strict';
+// 'use strict';
 
+// const mark = {
+//     fullName: 'Mark Miller',
+//     mass: 78,
+//     height: 1.69,
+//     calcBMI: function(){
+//         this.bmi = this.mass / this.height ** 2;
+//         return this.bmi;
+//     }
+// };
+
+// const john = {
+//     fullName: 'John Smith',
+//     mass: 92,
+//     height: 1.95,
+//     calcBMI: function(){
+//         this.bmi = this.mass / this.height ** 2;
+//         return this.bmi;
+//     }
+// };
+
+
+// mark.calcBMI();
+// john.calcBMI();
+
+// console.log(mark.bmi);
+// console.log(john.bmi);
+
+const calcTip = function (bill){
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52] 
+
+const tips = []
+const totals = []
+
+for(let i = 0; i < bills.length ; i++ ){
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]); 
+}
+
+console.log(bills, tips, totals)
+
+const calcAvg = function(arr){
+    let sum = 0
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i]
+    }
+    return sum / arr.length;
+}
+
+console.log(calcAvg([2,3,7]))
+console.log(calcAvg(totals))
